7859d8e3e6c66557a6911db0731e70ceb9db96f5
jbond21 <67006940+jbond21@users.noreply.github.com>
2020-12-03 13:28:57 -0500

    first commit

---
+++ json-api-app/Course.swift
@@ -0,0 +1,8 @@
+//
+//  Course.swift
+//  json-api-app
+//
+//  Created by Jordan Bond on 12/2/20.
+//
+
+import Foundation
---
+++ json-api-app/ScreenTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ScreenTableViewCell.swift
+//  json-api-app
+//
+//  Created by Jordan Bond on 12/3/20.
+//
+
+import UIKit
+
+class ScreenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
